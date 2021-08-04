23f009e251348eb7dbd16fd2045d88e77aac48b1
thatanjan <anjan.cules@gmail.com>
2021-08-04 10:43:18 +0600

    packages are installed

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -2,13 +2,11 @@ import * as React from 'react'
 import { CacheProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheets } from '@material-ui/styles'
 import createEmotionServer from '@emotion/server/create-instance'
 
-const getCache = () => {
+function getCache() {
 	const cache = createCache({ key: 'css', prepend: true })
 	cache.compat = true
-
 	return cache
 }
 
@@ -17,6 +15,7 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang='en'>
 				<Head>
+					{/* PWA primary color */}
 					<link
 						rel='stylesheet'
 						href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
@@ -56,8 +55,6 @@ MyDocument.getInitialProps = async (ctx) => {
 	// 3. app.render
 	// 4. page.render
 
-	// Render app and page and get the context of the page with collected side effects.
-	const sheets = new ServerStyleSheets()
 	const originalRenderPage = ctx.renderPage
 
 	const cache = getCache()
@@ -65,7 +62,6 @@ MyDocument.getInitialProps = async (ctx) => {
 
 	ctx.renderPage = () =>
 		originalRenderPage({
-			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
 			// Take precedence over the CacheProvider in our custom _app.js
 			// eslint-disable-next-line
 			enhanceComponent: (Component) => (props) => (
@@ -89,10 +85,6 @@ MyDocument.getInitialProps = async (ctx) => {
 	return {
 		...initialProps,
 		// Styles fragment is rendered after the app and page rendering finish.
-		styles: [
-			...React.Children.toArray(initialProps.styles),
-			sheets.getStyleElement(),
-			...emotionStyleTags,
-		],
+		styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
 	}
 }
