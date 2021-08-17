5a137fb64b197b4aa616c45b2163ed015824c688
thatanjan <anjan.cules@gmail.com>
2021-08-17 16:04:58 +0600

    refactored

--- server/src/validation/login.js
+++ server/src/validation/login.js
@@ -1,27 +1,27 @@
 import Validator from 'validator'
 import isEmpty from 'validation/is_empty'
 
-const validateLoginInput = data => {
-	const theData = data
+const validateLoginInput = (data) => {
+	const input = data
 	const errors = {}
 
 	const validatorProperties = ['email', 'password']
 
-	validatorProperties.forEach(property => {
-		theData[property] = !isEmpty(theData[property]) ? theData[property] : ''
-	})
+	for (let property in input) {
+		input[property] = !isEmpty(input[property]) ? input[property] : ''
+	}
 
-	if (!Validator.isEmail(theData.email)) {
+	if (!Validator.isEmail(input.email)) {
 		errors.email = 'invalid email address'
 		errors.email[0].toUpperCase()
 	}
 
-	if (Validator.isEmpty(theData.email)) {
+	if (Validator.isEmpty(input.email)) {
 		errors.email = 'email field is required'
 		errors.email[0].toUpperCase()
 	}
 
-	if (Validator.isEmpty(theData.password)) {
+	if (Validator.isEmpty(input.password)) {
 		errors.password = 'password is required'
 		errors.password[0].toUpperCase()
 	}
--- server/src/validation/register.js
+++ server/src/validation/register.js
@@ -13,36 +13,31 @@ const validateRegisterInput = (data) => {
 	})
 
 	if (!Validator.isLength(theData.name, { min: 3, max: 30 })) {
-		errors.name = 'Name must be between 2 and 30'
+		errors.name = 'Name must be between 3 and 30'
 	}
 
 	if (Validator.isEmpty(theData.name)) {
 		errors.name = 'name field is required'
-		errors.name[0].toUpperCase()
 	}
 
 	if (Validator.isEmpty(theData.email)) {
 		errors.email = 'email field is required'
-		errors.email[0].toUpperCase()
 	}
 
 	if (!Validator.isEmail(theData.email)) {
 		errors.email = 'invalid email address'
-		errors.email[0].toUpperCase()
 	}
 
 	if (Validator.isEmpty(theData.password)) {
 		errors.password = 'password is invalid'
-		errors.password[0].toUpperCase()
 	}
 
 	if (!Validator.isLength(theData.password, { min: 6, max: 30 })) {
-		errors.password = 'password must be at least 6 characters'
+		errors.password = 'password must be between 6 to 30 characters long'
 	}
 
 	if (Validator.isEmpty(theData.confirmPassword)) {
 		errors.confirmPassword = 'confirm password field is required'
-		errors.confirmPassword[0].toUpperCase()
 	}
 
 	if (!Validator.equals(theData.password, theData.confirmPassword)) {
